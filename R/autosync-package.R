15fd0e52428e7c2ecc48b31f2c963257d770bb94
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2026-06-30 20:45:39 +0100

    Roxygenize

--- R/autosync-package.R
+++ R/autosync-package.R
@@ -45,12 +45,17 @@
 #' @name autosync-package
 #' @aliases autosync
 #'
-#' @importFrom automerge am_create am_get am_keys am_length am_load am_save am_sync_decode am_sync_encode am_sync_state am_sync_state_decode am_sync_state_encode AM_ROOT
-#' @importFrom httr2 oauth_client oauth_flow_auth_code oauth_redirect_uri oauth_server_metadata
+#' @importFrom automerge am_create am_get am_keys am_length am_load am_save
+#'   am_sync_decode am_sync_encode am_sync_state am_sync_state_decode
+#'   am_sync_state_encode AM_ROOT
+#' @importFrom httr2 oauth_client oauth_flow_auth_code oauth_redirect_uri
+#'   oauth_server_metadata
 #' @importFrom jose jwt_decode_sig read_jwk
 #' @importFrom later later run_now
-#' @importFrom nanonext handler_ws http_server is_error_value ncurl random recv recv_aio send stop_aio stream tls_config unresolved write_cert
+#' @importFrom nanonext handler_ws http_server is_error_value ncurl random recv
+#'   recv_aio send stop_aio stream tls_config unresolved write_cert
 #' @importFrom promises then
-#' @importFrom secretbase base64enc base64dec base58enc base58dec cborenc cbordec jsondec jsonenc
+#' @importFrom secretbase base64enc base64dec base58enc base58dec cborenc
+#'   cbordec jsondec jsonenc
 #' @importFrom utils str
 "_PACKAGE"
